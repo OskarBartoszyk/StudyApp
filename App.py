fadb5cee081dec6b7170b29b49b1e3faaae331d8
Oskar Bartoszyk <oskarbartoszyk@gmail.com>
2023-08-12 21:35:18 +0200

    testing

--- App.py
+++ App.py
@@ -3,3 +3,13 @@ import datetime
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
+
+date_today = datetime.date.today()
+print(date_today)
+x= date_today
+y = int(input("yeye nene: "))
+
+plt.plot(x, y)
+plt.ylabel("Hours")
+plt.xlabel("Days")
+plt.show()
