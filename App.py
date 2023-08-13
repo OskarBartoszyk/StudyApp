35bdee16c06870babb2f55b5714992b4317178c3
Oskar Bartoszyk <oskarbartoszyk@gmail.com>
2023-08-13 10:03:07 +0200

    'fixing'

--- App.py
+++ App.py
@@ -4,12 +4,24 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-date_today = datetime.date.today()
-print(date_today)
-x= date_today
-y = int(input("yeye nene: "))
+input_data = input("Podaj liczby oddzielone spacją: ")
+numbers = list(map(int, input_data.split()))
 
-plt.plot(x, y)
+# Utworzenie dat dla osi x (obecne daty)
+current_date = datetime.datetime.now()
+dates = [current_date for i in range(len(numbers))]
+
+# Tworzenie wykresu
+plt.plot(dates, numbers, marker='o')
+
+# Konfiguracja osi x (daty)
+
+
+# Konfiguracja osi y (liczby z inputu)
+plt.ylim(0, 24)
 plt.ylabel("Hours")
 plt.xlabel("Days")
+
+# Wyświetlenie wykresu
+plt.tight_layout()
 plt.show()
